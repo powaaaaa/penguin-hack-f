7e69acfad8a1449ee42e1349270729c505ec78f1
catgolem <kugi1255@icloud.com>
2023-02-28 02:04:39 +0900

    [add] icons

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Login.css';
 
 const Page1 = () => {
     return (
         <div className='w-screen h-screen'>
-            <div className='flex justify-center items-center'>
-                <Link to="/.auth/login/github?post_login_redirect_uri=/Home">
-                    <img src='../img/github-mark.png'></img>
-                </Link>
-                <Link to="/.auth/login/twitter?post_login_redirect_uri=/Home">
-                    <img src='../img/Logoblue.svg'></img>
-                </Link>
-                <Link to="/.auth/login/aad?post_login_redirect_uri=/Home">
-                    <img src='../img/Azure-Active-Directory.svg'></img>
-                </Link>
+            <div className='flex justify-center box-icon'>
+                <a className='mr-24' href="http://localhost:4280/.auth/login/github?post_login_redirect_uri=/Home">
+                    <img className='h-24' src='../img/github-mark.svg'></img>
+                </a>
+                <a className='mx-24' href="/.auth/login/twitter?post_login_redirect_uri=/Home">
+                    <img className='h-24' src='../img/twitter.svg'></img>
+                </a>
+                <a className='ml-24' href="/.auth/login/aad?post_login_redirect_uri=/Home">
+                    <img className='h-24' src='../img/Azure-Active-Directory.svg'></img>
+                </a>
             </div>
         </div>
     );
