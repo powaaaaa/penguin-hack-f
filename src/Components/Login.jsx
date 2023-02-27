598e44ea750685cf01702c5df1bba9c97408cd09
catgolem <kugi1255@icloud.com>
2023-02-28 01:13:00 +0900

    [add] login page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,9 +3,17 @@ import { Link } from 'react-router-dom';
 
 const Page1 = () => {
     return (
-        <div>
+        <div className='w-screen h-screen'>
             <div className='flex justify-center items-center'>
-                <Link to="/Home">Home</Link>
+                <Link to="/.auth/login/github?post_login_redirect_uri=/Home">
+                    <img src='../img/github-mark.png'></img>
+                </Link>
+                <Link to="/.auth/login/twitter?post_login_redirect_uri=/Home">
+                    <img src='../img/Logoblue.svg'></img>
+                </Link>
+                <Link to="/.auth/login/aad?post_login_redirect_uri=/Home">
+                    <img src='../img/Azure-Active-Directory.svg'></img>
+                </Link>
             </div>
         </div>
     );
